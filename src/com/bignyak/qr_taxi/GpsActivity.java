70218ae9521bd4c5149e14dbc7b93499db6b296f
Dima Bignyak <dima.bignyak@bookrenter.com>
2013-12-03 16:14:16 +0200

    add dialog window to continue button

--- src/com/bignyak/qr_taxi/GpsActivity.java
+++ src/com/bignyak/qr_taxi/GpsActivity.java
@@ -1,7 +1,9 @@
 package com.bignyak.qr_taxi;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,6 +19,7 @@ public class GpsActivity extends Activity {
     public LocationManager lm;
     public LocationListener ll;
     public long lastRequest = 0;
+    public final Context context = this;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -80,5 +83,26 @@ public class GpsActivity extends Activity {
     public void getAddressManual(View view) {
         Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         updateLocation(location);
+    }
+
+    public void alertSendParams(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setTitle("Are you shure?");
+        alertDialogBuilder
+                .setMessage("Do you want to call taxi with this parameters?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // TODO: add here call function to send request to server
+                        GpsActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
